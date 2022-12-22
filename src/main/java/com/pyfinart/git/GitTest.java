813765b4e358b59dd19b0bf35346d4d0dfb61610
Levi <pyfinart@gmail.com>
2022-12-22 23:15:15 +0000

    first commit from idea

---
+++ src/main/java/com/pyfinart/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.pyfinart.git;
+
+/**
+ * @author pyfinart
+ * @create 2022-12-22-23:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
